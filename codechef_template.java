6647cd18c672f917f9a227c239e52306c62dcbee
lxduan <lxduan@gmail.com>
2013-08-25 19:45:04 +0800

    added a Java template for the codechef competition

---
+++ codechef_template.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        /**
+         * 1. read inputs, e.g. br.readLine()
+         * 2. solve()
+         * 3. output results, e.g. System.out.println(...)
+         */
+
+        // example
+        int num = br.readLine(); // read inputs
+		System.out.println(solve(num)); // solve and output
+    }
+
+    public static int solve(int input) {
+    	
+    }
+}
